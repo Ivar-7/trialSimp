7f730a593dccec971debb962a0a102299c287f22
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-20 20:12:02 +0300

    duplicate encode

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-// Make a function to add all numbers of an array
-console.log("Desktop tutorial.");
-  let sum = 0;
-  let sum = 0;
+function duplicateEncode(word) {
+  // Count the number of occurrences of each character in the word
+  const charCount = {};
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i].toLowerCase();
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Encode the string based on the character count
+  const result = [];
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i].toLowerCase();
+    result.push(charCount[char] > 1 ? ")" : "(");
+  }
+
+  return result.join("");
+}
+
+console.log(duplicateEncode("pomupomz"));
+console.log(duplicateEncode("Success"));
+// console.log("Success)");

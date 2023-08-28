2ee196a20b0e4dbfdb19f37513b1d940860eaecc
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-08-28 16:22:35 +0300

    x

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const arr = [7, 3, 7, 3, 7, 8, 3]
+// function to add all numbers in an array
 
-function solution(A) {
-    let obj = {}
-
-    for (let i = 0; i < A.length; i ++){
-        obj[A[i]] = (obj[A[i]] || 0) + 1
-    }
-    const result = Object.entries(obj).find(([key, value]) => value === 1);
-    console.log(result[0])
+function addArray(array) {
+    
 }
-
-solution(arr)
